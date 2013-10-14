f61a77604ee76e1e2d48defa51afdb2a4f693572
John Sandfort <garnavis@gmail.com>
2013-10-14 17:48:16 -0400

    Now he insults people.

--- src/johnpybot.py
+++ src/johnpybot.py
@@ -1,6 +1,7 @@
 import botlib
 import urllib
-import pytumblr 
+import pytumblr
+import csv
 from bs4 import BeautifulSoup
 from random import choice
 
@@ -13,14 +14,34 @@ class JohnPyBot(botlib.Bot):
         if password != None:
             self.protocol.privmsg("nickserv", "identify" % password)
 
+        self.commands = ["!hello",
+                         "!leave",
+                         "!tumblr",
+                         "!insult"]
+
         self.masters = []
         self.masters.append("Garnavis")
 
         self.client = pytumblr.TumblrRestClient( \
-                'sSMRBoKp2ognfANViuUGt6V4CfNDhTIVSC8agmORVGH5VDXTon')
+                "sSMRBoKp2ognfANViuUGt6V4CfNDhTIVSC8agmORVGH5VDXTon")
+
+        self.insults_a = []
+        self.insults_b = []
+        self.insults_c = []
+
+        with open("insults.txt", "rb") as tabfile:
+            self.insult_reader = csv.reader(tabfile, delimiter='\t')
+            for row in self.insult_reader:
+                self.insults_a.append(row[0])
+                self.insults_b.append(row[1])
+                self.insults_c.append(row[2])
 
     def __actions__(self):
         botlib.Bot.__actions__(self)
+
+        # List all commands.
+        if botlib.check_found(self.data, "!commands"):
+            self.protocol.privmsg(self.channel, ", ".join(self.commands))
 
         # Say hello to the prompting user.
         if botlib.check_found(self.data, "!hello"):
@@ -28,7 +49,8 @@ class JohnPyBot(botlib.Bot):
             self.protocol.privmsg(self.channel, "Hello %s!" % username)
 
         # Leave the network upon command.
-        if botlib.check_found(self.data, "!leave"):
+        if botlib.check_found(self.data, "!leave") or \
+           botlib.check_found(self.data, "!getout"):
             self.protocol.disconnect("If I must.")
 
         # Get the title from the web page linked.
@@ -36,14 +58,18 @@ class JohnPyBot(botlib.Bot):
            self.get_username() in self.masters:
             url = "http://" + self.data.partition("http://")[2]
             soup = BeautifulSoup(urllib.urlopen(url))
-            self.protocol.privmsg(self.channel, soup.title.string)
+            self.protocol.privmsg(self.channel, \
+                    soup.title.string.encode("utf-8", "ignore"))
 
+        # Add a user to the URL check list.
         if botlib.check_found(self.data, "!add"):
             self.masters.append(self.get_args()[0])
 
+        # Show the users whose URLs Lucien checks.
         if botlib.check_found(self.data, "!list"):
             self.protocol.privmsg(self.channel, self.masters)
 
+        # Find a recent text post with the given tag.
         if botlib.check_found(self.data, "!tumblr"):
             self.tag = " ".join(self.get_args())
             self.posts = self.client.tagged(self.tag, filter="text")
@@ -60,6 +86,14 @@ class JohnPyBot(botlib.Bot):
             else:
                 self.protocol.privmsg(self.channel, \
                         "Sorry, I can't find anything for that tag.")
+
+        # Say a Shakespearean insult.
+        if botlib.check_found(self.data, "!insult"):
+            self.protocol.privmsg(self.channel, \
+                    "Thou %s, %s %s!" % \
+                    (choice(self.insults_a), \
+                     choice(self.insults_b), \
+                     choice(self.insults_c)))
 
 if __name__== "__main__":
     # Create a new instance of our bot and run it
